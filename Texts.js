50427a544f1be7ed2e3d004f124ea5f78a00fb08
ZumTheZazaKing <73376015+ZumTheZazaKing@users.noreply.github.com>
2023-04-19 15:17:47 +0800

    Add files via upload

---
+++ Texts.js
@@ -0,0 +1,7 @@
+const texts = [
+    "Hello. I am a good boy!",
+    "You sure are a good boy!",
+    "I will kill all the bills",
+    "Dirty deeds done dirt cheap",
+    "I really wonder why you went to the other side of the gates"
+]
